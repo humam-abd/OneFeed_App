3619d71ee0f8e0e59d83d4f000d1e8b745d12a9b
Humam Abdullah <humamabdullah@outlook.com>
2021-05-14 16:12:33 +0530

    Card view and list view

--- src/components/NewsContainer/index.tsx
+++ src/components/NewsContainer/index.tsx
@@ -3,13 +3,14 @@ import { NewsArticle, NewsResponse } from "../../domain/models";
 import { useAsyncEffect } from "../../domain/utils";
 import { SearchBlock } from "../SearchBlock";
 import "../../styles/NewsContainer.scss";
+import { View } from "../../domain/enum";
 import { getNewsData } from "./NewsContainer.service";
 
 export const NewsContainer: React.FunctionComponent = () => {
-  // State initialization
   const [searchString, setSearchString] = useState("");
   const [date, setDate] = useState<string>("");
   const [newsData, setData] = useState<Array<NewsArticle>>([]);
+  const [view, setView] = useState(View.CardView);
 
   // Call api for news data
   useAsyncEffect(async () => {
@@ -29,30 +30,77 @@ export const NewsContainer: React.FunctionComponent = () => {
         date={date}
       />
       <hr />
+      <div className="d-flex">
+        <div className="me-2">
+          <i className="fas fa-list set-color"></i>
+        </div>
+        <label className="switch col-lg-12">
+          <input
+            type="checkbox"
+            value={view}
+            checked={view === View.CardView}
+            onChange={() => {
+              setView(view === View.CardView ? View.ListView : View.CardView);
+            }}
+          />
+          <span className="slider round"></span>
+        </label>
+        <div className="ms-2">
+          <i className="fas fa-th-large set-color"></i>
+        </div>
+      </div>
 
       <div className="row mt-4">
         {newsData?.map((item) => {
+          if (view === View.CardView) {
+            return (
+              <div className="col">
+                <div className="card custom-card-body custom-border-radius">
+                  {item.urlToImage && (
+                    <img
+                      src={item.urlToImage}
+                      className="card-img-top img-border-radius"
+                      alt="News"
+                    />
+                  )}
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">{item.description}</p>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="btn btn-secondary custom-border-radius"
+                    >
+                      Go to Story
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          }
           return (
-            <div className="col">
-              <div className="card custom-card-body custom-border-radius">
-                {item.urlToImage && (
-                  <img
-                    src={item.urlToImage}
-                    className="card-img-top img-border-radius"
-                    alt="News"
-                  />
-                )}
-                <div className="card-body">
-                  <h5 className="card-title">{item.title}</h5>
-                  <p className="card-text">{item.description}</p>
-                  <a
-                    href={item.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="btn btn-secondary custom-border-radius"
-                  >
-                    Go to Story
-                  </a>
+            <div
+              key={item.url}
+              className="card mb-3 custom-border-radius list-card"
+            >
+              <div className="d-flex">
+                <div className="float-start fill-img">
+                  {item.urlToImage && <img src={item.urlToImage} alt="News" />}
+                </div>
+                <div>
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">{item.description}</p>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="btn btn-secondary custom-border-radius"
+                    >
+                      Go to Story
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
